35f53ac3ce81ae293747260008070bc573d9b7b0
Josiel da Sillva Faria <josiel.faria@grupont.com.br>
2018-03-23 15:56:21 -0300

    Funcoes do banco de dados firebase

--- docs/js/app.js
+++ docs/js/app.js
@@ -1,43 +1,105 @@
+// curl 'https://[PROJECT_ID].firebaseio.com/users/jack/name.json
+
+// var config = {
+//     apiKey: "AIzaSyBMI6R5l6B4H2fzGAh68gSK5iGZ1GXk_yM",
+//     authDomain: "crudtarefa.firebaseapp.com",
+//     databaseURL: "https://crudtarefa.firebaseio.com",
+//     projectId: "crudtarefa",
+//     storageBucket: "crudtarefa.appspot.com",
+//     messagingSenderId: "175156036936"
+// };
+// firebase.initializeApp(config);
+
+//referencia do banco de dados firebase
 (function () {
     'use strict';
 
     angular.module('cadastro', [])
 
-        .controller('UsuarioController', function ($scope) {
+        .controller('tarefaController', function ($scope, $http) {
 
-            // lista de usuários pré cadastrados
-            $scope.usuarios = [
-                {nome: 'vazio', senha: '1234'},
-                {nome: 'Fulano', senha: '1234'},
-                {nome: 'Maria', senha: '12345'},
-                {nome: 'Zé', senha: '12345678'}
+            $scope.tarefas = [
+                { tarefa: 'vazio', descricao: '1234' },
+                { tarefa: 'Fulano', descricao: '1234' },
+                { tarefa: 'Maria', descricao: '12345' },
+                { tarefa: 'Zé', descricao: '12345678' }
             ];
 
             //inicialização de um usuário vazio
-            $scope.usuario = {nome: '', senha: ''};
+            $scope.tarefa = { tarefa: '', descricao: '' };
 
             //método para adicionar o usuário a lista
             $scope.cadastrar = function () {
-                $scope.usuarios.push($scope.usuario);
-                $scope.usuario = {nome: '', senha: ''};
-                $scope.statusSenha = {};
+                $scope.tarefas.push($scope.tarefa);
+                $scope.tarefa = { tarefa: '', descricao: '' };
+                $scope.statusdescricao = {};
             };
 
-            //método para validar a senha do usuário
-            $scope.validarSenha = function () {
-                $scope.statusSenha = {};
+            //método para buscar do BD firebase e popular a tabela
+            $scope.atualizar = function () {
 
-                if ($scope.usuario.senha && $scope.usuario.senha.length >= 6) {
-                    $scope.statusSenha.classe = 'has-success';
-                    $scope.statusSenha.icone = 'glyphicon-ok';
-                    $scope.statusSenha.mensagem = 'Senha forte';
+                // Definindo a referência com a qual trabalharemos: "text"
+                var textRef = firebase.database().ref('tarefas');
+                
+                // "Escutando" toda e qualquer alteração da chave "text"
+                textRef.on('value', function (snapshot) {
+                    // Escrevendo o que foi recebido em tempo real no nosso campo de texto
+                    let desc = snapshot.val().tarefa2.Descricao;
+                    let taref = snapshot.val().tarefa2.Tarefa;
 
-                }else {
-                    $scope.statusSenha.classe = 'has-error';
-                    $scope.statusSenha.icone = 'glyphicon-remove';
-                    $scope.statusSenha.mensagem = 'Senha fraca';
-                }
+                    console.log(snapshot.val().tarefa2.Descricao)
+                    console.log(snapshot.val())
+                    // document.getElementById('text').value = snapshot.val();
+
+                    $scope.tarefa = { tarefa: taref, descricao: desc };
+                    $scope.tarefas.push($scope.tarefa);
+                    $scope.tarefa = { tarefa: "", descricao: "" };
+                });
+
+                // var onSuccess = (data) => {
+                //     // console.log(data)
+                //     $scope.tarefas = data.data;
+                //     console.log($scope.tarefas)
+                //     return data;
+                // }
+                // var onFail = (data) => {
+                //     console.log('er ' + data.data)
+                //     alert('error')
+                // }
+                // var database = $http.get("https://crudtarefa.firebaseio.com/tarefas.json").then(onSuccess, onFail);
+                // console.log(database)
+                // database.val();
+                // database.val(function (snapshot) {
+
+                // var message = snapshot.val();
+                //$scope.tarefas.push(message);
+                // $scope.tarefas = snapshot */
+                // console.log(snapshot);
+
+                // $scope.tarefas = snapshot;
+                // });
+
+
             };
+            $scope.atualizar();
         });
-
 })();
+
+
+
+
+    //método para validar a senha do usuário
+            // $scope.validardescricao = function () {
+            //     $scope.statusdescricao = {};
+
+            //     if ($scope.tarefa.descricao && $scope.tarefa.descricao.length >= 6) {
+            //         $scope.statusdescricao.classe = 'has-success';
+            //         $scope.statusdescricao.icone = 'glyphicon-ok';
+            //         $scope.statusdescricao.mensagem = 'descricao forte';
+
+            //     } else {
+            //         $scope.statusdescricao.classe = 'has-error';
+            //         $scope.statusdescricao.icone = 'glyphicon-remove';
+            //         $scope.statusdescricao.mensagem = 'descricao fraca';
+            //     }
+            // };
--- docs/js/main.js
+++ docs/js/main.js
@@ -1,13 +1,57 @@
+//referencia do banco de dados firebase
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyBMI6R5l6B4H2fzGAh68gSK5iGZ1GXk_yM",
+    authDomain: "crudtarefa.firebaseapp.com",
+    databaseURL: "https://crudtarefa.firebaseio.com",
+    projectId: "crudtarefa",
+    storageBucket: "crudtarefa.appspot.com",
+    messagingSenderId: "175156036936"
+};
+firebase.initializeApp(config);
+
+
+//funcao que faz a inserção no Banco do FIREBASE
 function salvar() {
-    let nome = document.getElementById("nome").value;
-    let senha = document.getElementById("senha").value;
-    if (nome != "" && senha != "") {
+    let tarefa = document.getElementById("tarefa").value;
+    let descricao = document.getElementById("descricao").value;
+    let feedSuccess = $("#feedSuccess");
+    let feedError = $("#feedError");
+
+    if (tarefa != "" && descricao != "") {
         let texto = {//JSON
-            "Nome": nome,
-            "Senha": senha
+            "Tarefa": tarefa,
+            "Descricao": descricao
         };
-        var txtJson = JSON.stringify(texto);//convertendo o json para string
-        var blob = new Blob([txtJson], { type: "text/plain;charset=utf-8" });
-        saveAs(blob, nome + ".json");
+
+        try {
+            var content = {};
+            content['/tarefas/'+tarefa] = texto;
+            return firebase.database().ref().update(content);
+            mostraFeed('success');
+        } catch (error) {
+            mostraFeed('error');
+        }
+    }
+
+    //Funcao para mostras os feeds de status da inserção no BD
+    function mostraFeed(tipo_alert) {
+        switch (tipo_alert) {
+            case 'success':
+                feedSuccess.show(300);
+                setTimeout(function () { feedSuccess.hide(1000); }, 2000);
+                break
+            case 'error':
+                feedError.show(300);
+                setTimeout(function () { feedError.hide(1000); }, 2000);
+                break
+        }
     }
 }
+// database.on('child_added', function (snapshot) {
+//     var message = snapshot.val();
+
+//     // if(message.Tarefa == "asdasdas"){
+//         // console.log(message.Tarefa)
+//     // }
+// });
