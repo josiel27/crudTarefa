3bfe6aa14ea0e452cb225ca9a7b6440ec09122be
Josiel da Sillva Faria <josiel.faria@grupont.com.br>
2018-03-26 17:04:28 -0300

    Commit com att

--- docs/js/app.js
+++ docs/js/app.js
@@ -1,105 +1,89 @@
-// curl 'https://[PROJECT_ID].firebaseio.com/users/jack/name.json
-
-// var config = {
-//     apiKey: "AIzaSyBMI6R5l6B4H2fzGAh68gSK5iGZ1GXk_yM",
-//     authDomain: "crudtarefa.firebaseapp.com",
-//     databaseURL: "https://crudtarefa.firebaseio.com",
-//     projectId: "crudtarefa",
-//     storageBucket: "crudtarefa.appspot.com",
-//     messagingSenderId: "175156036936"
-// };
-// firebase.initializeApp(config);
 
 //referencia do banco de dados firebase
+let desc;
+let taref;
 (function () {
     'use strict';
 
     angular.module('cadastro', [])
 
         .controller('tarefaController', function ($scope, $http) {
-
-            $scope.tarefas = [
-                { tarefa: 'vazio', descricao: '1234' },
-                { tarefa: 'Fulano', descricao: '1234' },
-                { tarefa: 'Maria', descricao: '12345' },
-                { tarefa: 'Zé', descricao: '12345678' }
-            ];
+            $scope.tarefas = [];
+            $scope.nomeAlterado = [];
 
             //inicialização de um usuário vazio
             $scope.tarefa = { tarefa: '', descricao: '' };
 
             //método para adicionar o usuário a lista
             $scope.cadastrar = function () {
-                $scope.tarefas.push($scope.tarefa);
-                $scope.tarefa = { tarefa: '', descricao: '' };
-                $scope.statusdescricao = {};
+                salvar();//funcao do main.js para fazer a inserção no firebase
+                $scope.tarefas = [];
+                $scope.atualizar();
+            };
+
+            $scope.deletar = function (nomeTarefa) {
+                //remover da tabela
+                $scope.tarefas.forEach(function (e) {
+                    if(e.tarefa == nomeTarefa){
+                        $('#'+nomeTarefa).remove();
+                    }
+                });
+                remover(nomeTarefa);//funcao do main.js para fazer a exclusao no firebase
+            };
+
+            $scope.alterar = function (nomeTarefa, descTarefa) {
+                $scope.nomeAlterado = nomeTarefa;
+                $('#btnSalvar').removeClass('disabled');
+                // $('#btnCancel').removeClass('disabled');
+                $('#btnAdicionar').addClass('disabled');
+                $scope.tarefa = { tarefa: nomeTarefa, descricao: descTarefa};
+                mostrarCrudTarefa();
+            };
+
+            $scope.salvarAlteracao = function () {
+                update($scope.nomeAlterado);
+                let tarefa = $scope.nomeAlterado;
+                // let descricao = document.getElementById("descricao").value;
+                
+                $scope.tarefas.forEach(function (e) {
+                    if(e.tarefa == tarefa){
+                        e.tarefa = tarefa;
+                        // e.descricao = descricao;
+                    }
+                });
+                $('#btnSalvar').addClass('disabled');
+                // $('#btnCancel').addClass('disabled');
+                $('#btnAdicionar').removeClass('disabled');
+                $scope.tarefas = [];
+                $scope.atualizar();
+            };
+
+            $scope.cancelAlteracao = function (nomeTarefa, descTarefa) {
+                $scope.tarefa = { tarefa: "", descricao: "" };
+                $('#btnSalvar').addClass('disabled');
+                // $('#btnCancel').addClass('disabled');
+                $('#btnAdicionar').removeClass('disabled');
+                esconderCrudTarefa();
             };
 
             //método para buscar do BD firebase e popular a tabela
             $scope.atualizar = function () {
-
                 // Definindo a referência com a qual trabalharemos: "text"
                 var textRef = firebase.database().ref('tarefas');
-                
+                // var acoes = ;
+
                 // "Escutando" toda e qualquer alteração da chave "text"
-                textRef.on('value', function (snapshot) {
-                    // Escrevendo o que foi recebido em tempo real no nosso campo de texto
-                    let desc = snapshot.val().tarefa2.Descricao;
-                    let taref = snapshot.val().tarefa2.Tarefa;
-
-                    console.log(snapshot.val().tarefa2.Descricao)
-                    console.log(snapshot.val())
-                    // document.getElementById('text').value = snapshot.val();
-
-                    $scope.tarefa = { tarefa: taref, descricao: desc };
-                    $scope.tarefas.push($scope.tarefa);
+                textRef.once('value', function (snapshot) {
+                    snapshot.forEach(function (childSnapshot) {
+                        var value = childSnapshot.val();
+                        $scope.tarefa = { tarefa: value.Tarefa, descricao: value.Descricao };
+                        $scope.tarefas.push($scope.tarefa);
+                    });
                     $scope.tarefa = { tarefa: "", descricao: "" };
+                    $scope.$apply();
                 });
-
-                // var onSuccess = (data) => {
-                //     // console.log(data)
-                //     $scope.tarefas = data.data;
-                //     console.log($scope.tarefas)
-                //     return data;
-                // }
-                // var onFail = (data) => {
-                //     console.log('er ' + data.data)
-                //     alert('error')
-                // }
-                // var database = $http.get("https://crudtarefa.firebaseio.com/tarefas.json").then(onSuccess, onFail);
-                // console.log(database)
-                // database.val();
-                // database.val(function (snapshot) {
-
-                // var message = snapshot.val();
-                //$scope.tarefas.push(message);
-                // $scope.tarefas = snapshot */
-                // console.log(snapshot);
-
-                // $scope.tarefas = snapshot;
-                // });
-
-
             };
             $scope.atualizar();
-        });
+        })
 })();
 
-
-
-
-    //método para validar a senha do usuário
-            // $scope.validardescricao = function () {
-            //     $scope.statusdescricao = {};
-
-            //     if ($scope.tarefa.descricao && $scope.tarefa.descricao.length >= 6) {
-            //         $scope.statusdescricao.classe = 'has-success';
-            //         $scope.statusdescricao.icone = 'glyphicon-ok';
-            //         $scope.statusdescricao.mensagem = 'descricao forte';
-
-            //     } else {
-            //         $scope.statusdescricao.classe = 'has-error';
-            //         $scope.statusdescricao.icone = 'glyphicon-remove';
-            //         $scope.statusdescricao.mensagem = 'descricao fraca';
-            //     }
-            // };
--- docs/js/main.js
+++ docs/js/main.js
@@ -10,48 +10,107 @@ var config = {
 };
 firebase.initializeApp(config);
 
+let feedSuccess = $("#feedSuccess");
+let feedError = $("#feedError");
 
 //funcao que faz a inserção no Banco do FIREBASE
 function salvar() {
     let tarefa = document.getElementById("tarefa").value;
     let descricao = document.getElementById("descricao").value;
-    let feedSuccess = $("#feedSuccess");
-    let feedError = $("#feedError");
 
     if (tarefa != "" && descricao != "") {
         let texto = {//JSON
             "Tarefa": tarefa,
             "Descricao": descricao
         };
-
         try {
             var content = {};
-            content['/tarefas/'+tarefa] = texto;
-            return firebase.database().ref().update(content);
-            mostraFeed('success');
+            content['/tarefas/' + tarefa] = texto;
+            retorno = firebase.database().ref().update(content);
+            mostraFeed('success', 'Tarefa adicionada!');
+            return retorno;
         } catch (error) {
-            mostraFeed('error');
+            mostraFeed('error', 'Erro ao adicionar tarefa!');
         }
     }
 
-    //Funcao para mostras os feeds de status da inserção no BD
-    function mostraFeed(tipo_alert) {
-        switch (tipo_alert) {
-            case 'success':
-                feedSuccess.show(300);
-                setTimeout(function () { feedSuccess.hide(1000); }, 2000);
-                break
-            case 'error':
-                feedError.show(300);
-                setTimeout(function () { feedError.hide(1000); }, 2000);
-                break
+}
+
+//funcao que faz a exclusao no Banco do FIREBASE
+function remover(nomeTarefa) {
+    let texto = {//JSON
+        "Tarefa": tarefa,
+        "Descricao": descricao
+    };
+    try {
+        var content = {};
+        content['/tarefas/' + nomeTarefa] = {};
+        retorno = firebase.database().ref().update(content);
+        mostraFeed('success', 'Tarefa foi removida!');
+        return retorno;
+    } catch (error) {
+        mostraFeed('error', 'Erro ao remover tarefa!');
+    }
+}
+
+//funcao que faz a alteracao no Banco do FIREBASE
+function update(nomeAlterado) {
+    console.log(nomeAlterado)
+    let tarefa = document.getElementById("tarefa").value;
+    let descricao = document.getElementById("descricao").value;
+
+    if (tarefa != "" && descricao != "") {
+        let texto = {//JSON
+            "Tarefa": tarefa,
+            "Descricao": descricao
+        };
+        try {
+            var content = {};
+            content['/tarefas/' + nomeAlterado] = {};
+            content['/tarefas/' + tarefa] = texto;
+            retorno = firebase.database().ref().update(content);
+            mostraFeed('success', 'Tarefa foi alterada!');
+            return retorno;
+        } catch (error) {
+            mostraFeed('error', 'Erro ao alterar tarefa!');
         }
     }
 }
-// database.on('child_added', function (snapshot) {
-//     var message = snapshot.val();
 
-//     // if(message.Tarefa == "asdasdas"){
-//         // console.log(message.Tarefa)
-//     // }
-// });
+
+//Funcao para mostras os feeds de status da inserção no BD
+function mostraFeed(tipo_alert, msg_show) {
+    console.log('tipo_alert')
+    var novaMsg = document.createElement('strong');
+    novaMsg.textContent = msg_show;
+
+    var msgSuccess = $(document).find('#msgSuccess');
+    var msgError = $(document).find('#msgError');
+    if (msgSuccess) { msgSuccess.remove(); };
+    if (msgError) { msgError.remove(); };
+
+    switch (tipo_alert) {
+        case 'success':
+            novaMsg.id = 'msgSuccess';
+            feedSuccess.append(novaMsg);
+            feedSuccess.show(300);
+            setTimeout(function () { feedSuccess.hide(1000); }, 2000);
+            break
+        case 'error':
+            novaMsg.id = 'msgError';
+            feedError.append(novaMsg);
+            feedError.show(300);
+            setTimeout(function () { feedError.hide(1000); }, 2000);
+            break
+    }
+}
+
+function mostrarCrudTarefa() {
+    $('#btnNovaTarefa').hide();
+    $('#crudForm').show(300);
+}
+
+function esconderCrudTarefa(){
+    $('#crudForm').hide(300);
+    $('#btnNovaTarefa').show();
+}
